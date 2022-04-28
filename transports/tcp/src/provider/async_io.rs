fe0eb8a4c0ee195294bd63147269d7ddced0e9d9
Wink Saville <wink@saville.com>
2022-04-28 22:17:46 +0000

    Reduce debug output
    Cargo.toml
     - Use local copies of netlink and polling
    
    transports/tcp/src/provider/async_io.rs
     - Changed all debug! to trace!

--- transports/tcp/src/provider/async_io.rs
+++ transports/tcp/src/provider/async_io.rs
@@ -35,21 +35,21 @@ impl Provider for Tcp {
     type IfWatcher = if_watch::IfWatcher;
 
     fn if_watcher() -> BoxFuture<'static, io::Result<Self::IfWatcher>> {
-        log::debug!("Tcp::Provider::if_watcher:+");
+        log::trace!("Tcp::Provider::if_watcher:+");
         let result = if_watch::IfWatcher::new().boxed();
-        log::debug!("Tcp::Provider::if_watcher:-");
+        log::trace!("Tcp::Provider::if_watcher:-");
         result
     }
 
     fn new_listener(l: net::TcpListener) -> io::Result<Self::Listener> {
-        log::debug!("Tcp::Provider::inew_listener:+");
+        log::trace!("Tcp::Provider::inew_listener:+");
         let result = Async::new(l);
-        log::debug!("Tcp::Provider::inew_listener:-");
+        log::trace!("Tcp::Provider::inew_listener:-");
         result
     }
 
     fn new_stream(s: net::TcpStream) -> BoxFuture<'static, io::Result<Self::Stream>> {
-        log::debug!("Tcp::Provider::new_stream:+");
+        log::trace!("Tcp::Provider::new_stream:+");
         let result = async move {
             // Taken from [`Async::connect`].
 
@@ -65,7 +65,7 @@ impl Provider for Tcp {
             }
         }
         .boxed();
-        log::debug!("Tcp::Provider::new_stream:-");
+        log::trace!("Tcp::Provider::new_stream:-");
         result
     }
 
@@ -73,48 +73,48 @@ impl Provider for Tcp {
         l: &mut Self::Listener,
         cx: &mut Context<'_>,
     ) -> Poll<io::Result<Incoming<Self::Stream>>> {
-        log::debug!("Tcp::Provider::poll_accept:+ tid={} incomming", std::thread::current().id().as_u64());
+        log::trace!("Tcp::Provider::poll_accept:+ tid={} incomming", std::thread::current().id().as_u64());
         let (stream, remote_addr) = loop {
             // This is the "higher level receiving" side of accepting a connection.
             // This doesn't happen when libp2p-lookup fails. I'm trying to get closer
             // to that point where it fails. Basically trying to determine if the lower
             // level code (epoll?) is never deliverying or this higher level code can't
             // accept it.
-            log::debug!("Tcp::Provider::poll_accept: call poll_readable cx={:?}", cx);
+            log::trace!("Tcp::Provider::poll_accept: call poll_readable cx={:?}", cx);
             let prr = l.poll_readable(cx);
-            log::debug!("Tcp::Provider::poll_accept: retf poll_readable prr={:?}", prr);
+            log::trace!("Tcp::Provider::poll_accept: retf poll_readable prr={:?}", prr);
             match prr {
                 Poll::Pending => {
                     let result = Poll::Pending;
-                    log::debug!("Tcp::Provider::poll_accept:- result.is_ready(): {}", result.is_ready());
+                    log::trace!("Tcp::Provider::poll_accept:- result.is_ready(): {}", result.is_ready());
                     return result
                 }
                 Poll::Ready(Err(err)) => {
                     let err_string: String = err.to_string();
                     let result = Poll::Ready(Err(err));
-                    log::debug!("Tcp::Provider::poll_accept:- result.is_ready(): {}, err: {}", result.is_ready(), err_string);
+                    log::trace!("Tcp::Provider::poll_accept:- result.is_ready(): {}, err: {}", result.is_ready(), err_string);
                     return result;
                 }
                 Poll::Ready(Ok(())) => {
-                    log::debug!("Tcp::Provider::poll_accept: call accept()");
+                    log::trace!("Tcp::Provider::poll_accept: call accept()");
                     let accept_result= l.accept();
-                    log::debug!("Tcp::Provider::poll_accept: retf accept()");
-                    log::debug!("Tcp::Provider::poll_accept: call now_or_never()");
+                    log::trace!("Tcp::Provider::poll_accept: retf accept()");
+                    log::trace!("Tcp::Provider::poll_accept: call now_or_never()");
                     let now_or_never_result = accept_result.now_or_never();
-                    log::debug!("Tcp::Provider::poll_accept: retf now_or_never() now_or_never_result={:?}", now_or_never_result);
+                    log::trace!("Tcp::Provider::poll_accept: retf now_or_never() now_or_never_result={:?}", now_or_never_result);
                     match now_or_never_result {
                         Some(Err(e)) => {
                             let err_string: String = e.to_string();
                             let result = Poll::Ready(Err(e));
-                            log::debug!("Tcp::Provider::poll_accept:- result.is_ready(): {}, err: {}", result.is_ready(), err_string);
+                            log::trace!("Tcp::Provider::poll_accept:- result.is_ready(): {}, err: {}", result.is_ready(), err_string);
                             return result;
                         }
                         Some(Ok(res)) => {
-                            log::debug!("Tcp::Provider::poll_accept: accepted res: {:?}", res);
+                            log::trace!("Tcp::Provider::poll_accept: accepted res: {:?}", res);
                             break res
                         }
                         None => {
-                            log::debug!("Tcp::Provider::poll_accept: None, continue looping");
+                            log::trace!("Tcp::Provider::poll_accept: None, continue looping");
                             // Since it doesn't do any harm, account for false positives of
                             // `poll_readable` just in case, i.e. try again.
                         }
@@ -130,25 +130,25 @@ impl Provider for Tcp {
             local_addr,
             remote_addr,
         }));
-        log::debug!("Tcp::Provider::poll_accept:- incoming result.is_ready():, {} local_addr: {}, remote_addr: {}", result.is_ready(), local_addr, remote_addr);
+        log::trace!("Tcp::Provider::poll_accept:- incoming result.is_ready():, {} local_addr: {}, remote_addr: {}", result.is_ready(), local_addr, remote_addr);
         result
     }
 
     fn poll_interfaces(w: &mut Self::IfWatcher, cx: &mut Context<'_>) -> Poll<io::Result<IfEvent>> {
-        log::debug!("Tcp::Provider::poll_interfaces:+");
+        log::trace!("Tcp::Provider::poll_interfaces:+");
 
         let result = w.poll_unpin(cx).map_ok(|e| match e {
             if_watch::IfEvent::Up(a) => {
-                log::debug!("Tcp::Provider::poll_interfaces call IfEvent::Up {}", a);
+                log::trace!("Tcp::Provider::poll_interfaces call IfEvent::Up {}", a);
                 IfEvent::Up(a)
             }
             if_watch::IfEvent::Down(a) => {
-                log::debug!("Tcp::Provider::poll_interfaces call IfEvent::Down {}", a);
+                log::trace!("Tcp::Provider::poll_interfaces call IfEvent::Down {}", a);
                 IfEvent::Down(a)
             }
         });
 
-        log::debug!("Tcp::Provider::poll_interfaces:- result.is_ready(): {}", result.is_ready());
+        log::trace!("Tcp::Provider::poll_interfaces:- result.is_ready(): {}", result.is_ready());
         result
     }
 }
